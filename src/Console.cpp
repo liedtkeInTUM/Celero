d31c5bc684e5729f53fc29b19ed33b0a3edf0c5a
Sven Liedtke <liedtke@in.tum.de>
2016-11-03 21:33:12 +0100

    [Style] Remove trailing whitespaces

--- src/Console.cpp
+++ src/Console.cpp
@@ -1,14 +1,14 @@
 ///
 /// \author	John Farrier
 ///
-/// \copyright Copyright 2016 John Farrier 
+/// \copyright Copyright 2016 John Farrier
 ///
 /// Licensed under the Apache License, Version 2.0 (the "License");
 /// you may not use this file except in compliance with the License.
 /// You may obtain a copy of the License at
-/// 
+///
 /// http://www.apache.org/licenses/LICENSE-2.0
-/// 
+///
 /// Unless required by applicable law or agreed to in writing, software
 /// distributed under the License is distributed on an "AS IS" BASIS,
 /// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -31,9 +31,9 @@ using namespace celero;
 #ifdef WIN32
 auto WinColor() -> decltype(GetStdHandle(STD_OUTPUT_HANDLE))
 {
-	auto h = GetStdHandle(STD_OUTPUT_HANDLE); 
-	CONSOLE_SCREEN_BUFFER_INFO csbiInfo; 
-	GetConsoleScreenBufferInfo(h, &csbiInfo); 
+	auto h = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_SCREEN_BUFFER_INFO csbiInfo;
+	GetConsoleScreenBufferInfo(h, &csbiInfo);
 	return h;
 }
 #endif
@@ -199,7 +199,7 @@ void Default()
 
 void celero::console::SetConsoleColor(const celero::console::ConsoleColor x)
 {
-	switch(x) 
+	switch(x)
 	{
 		case ConsoleColor_Red:				Red();				break;
 		case ConsoleColor_Red_Bold:			RedBold();			break;
@@ -215,7 +215,7 @@ void celero::console::SetConsoleColor(const celero::console::ConsoleColor x)
 		case ConsoleColor_White_Bold:		WhiteBold();		break;
 		case ConsoleColor_WhiteOnRed:		WhiteOnRed(); 		break;
 		case ConsoleColor_WhiteOnRed_Bold:	WhiteOnRedBold();	break;
-		case ConsoleColor_Purple_Bold:		PurpleBold();		break;	
+		case ConsoleColor_Purple_Bold:		PurpleBold();		break;
 		case ConsoleColor_Default:
 		default:							Default();			break;
 	}
